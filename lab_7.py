9e80a6d9ea3199d4c62df3718ff3eb54cf934e41
nardeen21-meet <nardeen21@meet.mit.edu>
2019-07-30 12:29:55 +0300

    Stopping here

--- lab_7.py
+++ lab_7.py
@@ -51,25 +51,22 @@ LEFT_EDGE = -400
 
 def up():
     snake.direction="Up"
-    move_snake()
+    
     print("you pressed the up key!")
     
 
 def down():
     snake.direction="Down"
-    move_snake()
     print("you pressed the down key!")
     
 
 def left():
     snake.direction="Left"
-    move_snake()
     print("you pressed the left key!")
 
 
 def right():
     snake.direction="Right"
-    move_snake()
     print("you pressed the right key!")
 
 
@@ -80,14 +77,30 @@ turtle.onkeypress(right, "Right")
 
 turtle.listen()
 
+turtle.register_shape("trash.gif")
+food = turtle.clone()
+food.shape("trash.gif")
+food_pos = [(100,100),(-100,100), (-100,-100), (100,-100)]
+food_stamps = []
+
+for this_food_pos in food_pos:
+    
+    food.goto(this_food_pos)
+    foodid = food.stamp()
+    food_stamps.append(foodid)
+    food.hideturtle()
+
+
 
 
 
 def move_snake():
-
+    x_pos = snake.xcor()
+    y_pos = snake.ycor()
     if snake.direction == "Up":
         snake.goto(x_pos, y_pos + SQUARE_SIZE)
         print("you moved up!")
+
     elif snake.direction=="Down":
         snake.goto(x_pos, y_pos - SQUARE_SIZE)
 
@@ -100,29 +113,38 @@ def move_snake():
   #Make the snake stamp a new square on the screen
     #Hint - use a single function to do this
     new_stamp()
+    if snake.pos() in food_pos:
+        food_index=food_pos.index(snake.pos())
+        food.clearstamp(food_stamps[food_index])
+        food_pos.pop(food_index)
+        print("you have eaten the food!")
+        
     remove_tail()
-
-def move_snake ():
+    
     
     my_pos = snake.pos()
     x_pos = my_pos[0]
     y_pos = my_pos[1]
 
-    if new_x_pos >= RIGHT_EDGE:
+    if my_pos[0] >= RIGHT_EDGE:
+        
          print("You hit the right edge! Game over!")
          quit()
 
-    elif new_x_pos <= LEFT_EDGE:
+    elif my_pos[0] <= LEFT_EDGE:
          print("You hit the left edge! Game over!")
          quit()
          
-    elif new_x_pos >= UP_EDGE:
+    elif my_pos[1] >= UP_EDGE:
          print("You hit the up edge! Game over!")
          quit()
-    elif new_x_pos <= DOWN_EDGE:
+    elif my_pos[1] <= DOWN_EDGE:
         print("you hit the down edge! Game over!")
         quit()
-
+        
+    turtle.ontimer(move_snake,TIME_STEP)
+    
+move_snake()
 
     
 turtle.mainloop()
